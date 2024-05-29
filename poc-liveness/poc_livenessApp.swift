6de69b8ab80dc346435a13fec9bb43219cd91eb2
davibatista1 <lbdavi1@gmail.com>
2024-05-29 10:05:47 -0300

    Initial Commit

---
+++ poc-liveness/poc_livenessApp.swift
@@ -0,0 +1,17 @@
+//
+//  poc_livenessApp.swift
+//  poc-liveness
+//
+//  Created by Davi Batista on 29/05/24.
+//
+
+import SwiftUI
+
+@main
+struct poc_livenessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ poc-livenessUITests/poc_livenessUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  poc_livenessUITestsLaunchTests.swift
+//  poc-livenessUITests
+//
+//  Created by Davi Batista on 29/05/24.
+//
+
+import XCTest
+
+final class poc_livenessUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
